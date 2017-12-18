9544cea019fa713bbeffce13d254c0e2c8d84b5d
carballoj <33456355+carballoj@users.noreply.github.com>
2017-12-18 15:28:37 +0100

    Update Servo control.cpp

--- Servo control.cpp
+++ Servo control.cpp
@@ -1,12 +1,60 @@
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include <MeMegaPi.h>
+
+double servo_speed=15;
+double angle_rad = PI/180.0;
+double angle_deg = 180.0/PI;
+MeSmartServo mysmartservo(PORT5);
+float getServoValue(int index, int type)
+{
+    	if(type==0)
+    	{
+        		return mysmartservo.getSpeedRequest(index);
+    	}
+    	else if(type==1)
+    	{
+        		return mysmartservo.getTempRequest(index);
+    	}
+    	else if(type==2)
+    	{
+        		return mysmartservo.getCurrentRequest(index);
+    	}
+    	else if(type==3)
+    	{
+        		return mysmartservo.getVoltageRequest(index);
+    	}
+    	else if(type==4)
+    	{
+        		return mysmartservo.getAngleRequest(index);
+    	}
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+void setup(){
+    //Set Pwm 8KHz
+    TCCR1A = _BV(WGM10);
+    TCCR1B = _BV(CS11) | _BV(WGM12);
+    TCCR2A = _BV(WGM21) | _BV(WGM20);
+    TCCR2B = _BV(CS21);
+    mysmartservo.begin(115200);
+    mysmartservo.assignDevIdRequest();
+    delay(50);
+    mysmartservo.begin(115200);
+    mysmartservo.assignDevIdRequest();    mysmartservo.moveTo(1,-45,servo_speed);
+}
+
+void loop(){
+    if(((getServoValue(1,4))==(-45))){
+        mysmartservo.move(1,90,servo_speed);
+    }
+    if(((getServoValue(1,4))==(45))){
+        mysmartservo.move(1,-90,servo_speed);
+    }
+    Serial.println(getServoValue(1,4));
+    _loop();
+}
+
+  
+void _loop(){
 }
